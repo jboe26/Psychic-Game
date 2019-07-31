7c9d4856051a05c15ea78b6e6e92f9300b0abbc7
jboe26 <joshrboepple@outlook.com>
2019-07-30 20:14:41 -0400

    updated the wins/losses

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,5 @@
 // Creates an array that lists out all of the options (Letters to guess).
-var wins = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+var wins = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var alreadyGuessed = []
 
 // Creating variables to hold the number of wins, losses, guesses left and guesses so far. They start at 0.
@@ -16,11 +16,15 @@ var GuessesLeftText = document.getElementById("guesses-left-text");
 var GuessesSoFarText = document.getElementById("guesses");
 console.log(GuessesSoFarText)
 
+ // Randomly chooses a choice from the options array. Guesses so far and guesses left.
+ var computerGuess = wins[Math.floor(Math.random() * wins.length)];
+
+ 
 // This function is run whenever the user presses a key.
 document.onkeyup = function (event) {
-    
-    
-    
+
+
+
     // Determines which key was pressed.
     var userGuess = event.key;
     alreadyGuessed.push(userGuess);
@@ -31,16 +35,9 @@ document.onkeyup = function (event) {
 
     lossesText.textContent = losses
 
-
-
-    
-
-    
-    // Randomly chooses a choice from the options array. Guesses so far and guesses left.
-    var computerGuess = wins[Math.floor(Math.random() * wins.length)];
-
+   
     if (userGuess === computerGuess) {
-            winsCount++;
+        winsCount++;
         alreadyGuessed = []
         guessesLeft = 10
     }
@@ -49,14 +46,12 @@ document.onkeyup = function (event) {
     else {
         guessesLeft--;
     }
-    if  (guessesLeft === 0) {
+    if (guessesLeft === 0) {
         losses++;
         guessesLeft = 10
         alreadyGuessed = []
-        
-    }
-};
- 
 
-
-
+        console.log(userGuess);
+        console.log(computerGuess);
+    
+    }}
